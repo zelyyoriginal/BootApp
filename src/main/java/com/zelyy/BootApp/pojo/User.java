db139741a2d7ce7a5d0b5d7a8b7a1f9a3a4fc30b
Zelyyoriginal <danilazelenskij97@gmail.com>
2024-10-13 10:26:27 +0300

    use lombok

--- src/main/java/com/zelyy/BootApp/pojo/User.java
+++ src/main/java/com/zelyy/BootApp/pojo/User.java
@@ -1,9 +1,15 @@
 package com.zelyy.BootApp.pojo;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,48 +20,4 @@ public class User {
     private String surname;
     @Column
     private String email;
-
-
-    public User() {
-    }
-
-
-    public User(String name, String surname, String email) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
