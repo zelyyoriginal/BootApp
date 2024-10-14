6318e3a6465ed187d54cbcd6b9c2f2d55f93a9d7
Zelyyoriginal <danilazelenskij97@gmail.com>
2024-10-14 11:26:38 +0300

    fix

---
+++ src/main/java/com/zelyy/BootApp/services/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.zelyy.BootApp.services;
+
+import com.zelyy.BootApp.pojo.User;
+import com.zelyy.BootApp.repositiry.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    private UserRepo userRepo;
+
+    @Autowired
+    public UserServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public void add(User user) {
+        userRepo.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void delete(Long id) {
+        userRepo.delete(userRepo.findById(id).get());
+    }
+
+    public User findById(Long id) {
+        return userRepo.findById(id).get();
+    }
+}
